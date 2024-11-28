636c1291d1b1ac890b2c2fe5cbcadf9471b3fae7
Airondon <anaisabelrondonrjas@gmail.com>
2024-11-28 12:59:44 -0500

    add module profile

--- messaging/models.py
+++ messaging/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=150, unique=True)
+    profile_picture = models.ImageField(upload_to="profile_picture/", blank=True, null=True)
 
 
 class Message(models.Model):
--- messaging/views.py
+++ messaging/views.py
@@ -32,3 +32,40 @@ def create_message(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["PUT"])
+def update_profile_picture(request, author_id):
+    try: 
+        author = Author.objects.get(id=author_id)
+    except Author.DoesNotExist:
+        return Response(
+            {
+                "error": "Autor no encontrado"
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
+    serializer = Authorserializer(author, data=request.data, partial=True)
+
+    if serializer.is_valid(): 
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def get_author_by_username(request, username):
+    try: 
+        
+        author = Author.objects.get(name=username)
+    except Author.DoesNotExist:
+        return Response(
+            {
+                "error": "Autor no encontrado"
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    
+    serializer = Authorserializer(author)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
